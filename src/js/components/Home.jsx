0185cf9220f6aef449dc423676aad434a1e8849c
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-03-04 15:39:21 +0000

    Remove unused code
    Move some to Test.jsx in to save for later (in case)

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -42,24 +42,6 @@ const deleteAllTasksWithAPI = () => {
   }
 };
 
-const getAllUsers = () => {
-  fetch(url + "/users") // URL for get/post/etc. request goes here
-    // Initial response to the fetch. If it goes good, it goes to the next .then. NOTE: You can leave this blank for GET requests.
-    .then(
-      // The response goes here
-      (resp) => {
-        // console.log("get all users - response:", resp.json());
-        return resp.json();
-      },
-    )
-    // Final result of the request
-    .then((data) => {
-      console.log("Get all users - data:", resp);
-    });
-};
-
-// let copyOfTasksInAPI = [{ label: "", is_done: false, id: 999 }];
-
 const Home = () => {
   // New task is empty string until user types and submits it to the tasks array
   const [newTask, setNewTask] = useState("");
@@ -123,13 +105,11 @@ const Home = () => {
     addTaskWithAPI(newTask);
   }
 
-  // Delete a specific task
   const deleteTask = (taskToDelete) => {
     const filteredTasks = tasks.filter((taskData) => taskData != taskToDelete);
     setTasks(filteredTasks);
   };
 
-  // User can input text in a field and click button or press enter to add typed task to tasks array
   return (
     <div className="container">
       <h1>Today's Task List</h1>
@@ -158,25 +138,21 @@ const Home = () => {
       />
       <button onClick={() => whenSaveBtnClicked()}>Save task</button>
       <ul>
-        {
-          // Print each task as a li
-          tasks.map((item, index) => {
-            return (
-              <li key={index + "task"}>
-                {item.label}
-                <button
-                  onClick={() => {
-                    // Delete a selected task
-                    deleteTask(item);
-                    deleteTaskWithAPI(item.id);
-                  }}
-                >
-                  X
-                </button>
-              </li>
-            );
-          })
-        }
+        {tasks.map((item, index) => {
+          return (
+            <li key={index + "task"}>
+              {item.label}
+              <button
+                onClick={() => {
+                  deleteTask(item);
+                  deleteTaskWithAPI(item.id);
+                }}
+              >
+                X
+              </button>
+            </li>
+          );
+        })}
         {
           // Message if there are no tasks
           tasks.length == 0 && <li className="text-success">All clear!</li>
@@ -186,7 +162,6 @@ const Home = () => {
           tasks.length > 0 && (
             <button
               onClick={() => {
-                // Delete all tasks by clearing tasks array
                 setTasks([]);
                 deleteAllTasksWithAPI();
               }}
---
+++ src/js/components/Test.jsx
@@ -0,0 +1,19 @@
+// Code that isn't being used in the other components but is saved here in case it needs to be moved to another component
+
+const getAllUsers = () => {
+  fetch(url + "/users") // URL for get/post/etc. request goes here
+    // Initial response to the fetch. If it goes good, it goes to the next .then. NOTE: You can leave this blank for GET requests.
+    .then(
+      // The response goes here
+      (resp) => {
+        // console.log("get all users - response:", resp.json());
+        return resp.json();
+      },
+    )
+    // Final result of the request
+    .then((data) => {
+      console.log("Get all users - data:", resp);
+    });
+};
+
+// let copyOfTasksInAPI = [{ label: "", is_done: false, id: 999 }];
